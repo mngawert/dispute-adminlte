63334155352a37e87f20d5377b7bb938d6f4b6d1
Montri <montrin@gmail.com>
2024-10-18 23:05:25 +0700

    Fix Sidebar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import DisputeData from "./components/DisputeData";
-import Sidebar from "./components/SideBar";
+import Sidebar from "./components/Sidebar";
 import Login from "./pages/Login";
 import Test from "./pages/Test";
 import PrivateRoute from "./components/PrivateRoute";
