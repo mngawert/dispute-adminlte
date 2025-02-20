0329c6aa1355c4aaa7aec541b06f1306c977a78a
Montri <montrin@gmail.com>
2025-02-20 20:48:26 +0700

    Update handleTabSelect.

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -5,235 +5,132 @@ import { Tab, Tabs } from 'react-bootstrap';
 import ReviewDocType from './ReviewDocType';
 import ContentHeader from '../components/ContentHeader';
 
-const Review = ({reviewType, prevDocumentStatus}) => {
+const Review = ({ reviewType, prevDocumentStatus }) => {
 
-    console.log(reviewType);
-    console.log(prevDocumentStatus);
+  console.log(reviewType);
+  console.log(prevDocumentStatus);
 
-    const [documentNum, setDocumentNum] = useState('');
-    const [documents, setDocuments] = useState([]);
-    const [selectedDocument, setSelectedDocument] = useState({});
-    const [adjustmentRequests, setAdjustmentRequests] = useState([]);
+  const [documentNum, setDocumentNum] = useState('');
+  const [documents, setDocuments] = useState([]);
+  const [selectedDocument, setSelectedDocument] = useState({});
+  const [adjustmentRequests, setAdjustmentRequests] = useState([]);
 
-    const [activeTab, setActiveTab] = useState('P36');
+  const [activeTab, setActiveTab] = useState('P36');
 
-    const filterDocumentsByType = (type) => {
-        return documents.filter(doc => doc.documentTypeDesc === type);
-    };
-    
-    const handleInputChange = (e) => {
-        setDocumentNum(e.target.value);
-    };
+  const filterDocumentsByType = (type) => {
+    return documents.filter(doc => doc.documentTypeDesc === type);
+  };
 
-    const getAllDocuments = async () => {
-        try {
-            const response = await api.get(`/api/Document/GetAllDocuments`, {
-                params: {
-                    documentStatus: prevDocumentStatus,
-                    documentNum: documentNum
-                }
-            });
-            setDocuments(response.data);
-        } catch (error) {
-            console.error('Error fetching data', error);
+  const handleInputChange = (e) => {
+    setDocumentNum(e.target.value);
+  };
+
+  const getAllDocuments = async () => {
+    try {
+      const response = await api.get(`/api/Document/GetAllDocuments`, {
+        params: {
+          documentStatus: prevDocumentStatus,
+          documentNum: documentNum
         }
-    };
-
-    const handleSearch = async () => {
-        getAllDocuments();
-    };   
-
-    const handleSelectDocument = async (doc) => {
-        setSelectedDocument(doc);
-
-        try {
-            const response = await api.get(`/api/Adjustment/GetAdjustmentRequests`, {
-                params: {
-                    documentNum: doc.documentNum
-                }
-            });
-            console.log(response.data);
-            setAdjustmentRequests(response.data);
-        } catch (error) {
-            console.error('Error fetching adjustment requests', error);
-        }
+      });
+      setDocuments(response.data);
+    } catch (error) {
+      console.error('Error fetching data', error);
     }
+  };
 
-    const handleUpdateDocumentStatus = async (doc, _documentStatus) => {
-        try {
-            const response = await api.put(`/api/Document/UpdateDocument${reviewType}Status/${doc.documentNum}`, {
-                documentNum: doc.documentNum,
-                documentStatus: _documentStatus,
-                updatedBy: JSON.parse(localStorage.getItem('userLogin'))?.userId
-            });
-            console.log(response.data);
-            //setDocuments(documents.map(d => d.documentNum === doc.documentNum ? {...d, documentStatus: _documentStatus} : d));
-            resetPage();
-            getAllDocuments();
-        } catch (error) {
-            console.error('Error updating document status', error);
+  const handleSearch = async () => {
+    getAllDocuments();
+  };
+
+  const handleSelectDocument = async (doc) => {
+    setSelectedDocument(doc);
+
+    try {
+      const response = await api.get(`/api/Adjustment/GetAdjustmentRequests`, {
+        params: {
+          documentNum: doc.documentNum
         }
+      });
+      console.log(response.data);
+      setAdjustmentRequests(response.data);
+    } catch (error) {
+      console.error('Error fetching adjustment requests', error);
     }
+  }
 
-    const resetPage = () => {
-        setDocumentNum('');
-        setDocuments([]);
-        setSelectedDocument({});
-        setAdjustmentRequests([]);
-      };
+  const handleUpdateDocumentStatus = async (doc, _documentStatus) => {
+    try {
+      const response = await api.put(`/api/Document/UpdateDocument${reviewType}Status/${doc.documentNum}`, {
+        documentNum: doc.documentNum,
+        documentStatus: _documentStatus,
+        updatedBy: JSON.parse(localStorage.getItem('userLogin'))?.userId
+      });
+      console.log(response.data);
+      resetPage();
+      getAllDocuments();
+    } catch (error) {
+      console.error('Error updating document status', error);
+    }
+  }
 
-    return(
-        <div className="content-wrapper-x">
-        {/* Content Header (Page header) */}
+  const resetPage = () => {
+    setDocumentNum('');
+    setDocuments([]);
+    setSelectedDocument({});
+    setAdjustmentRequests([]);
+  };
 
-        <ContentHeader title={reviewType + " Adjustments"} />
+  const handleTabSelect = (k) => {
+    setActiveTab(k);
+    setSelectedDocument({});
+    setAdjustmentRequests([]);
+  };
 
-        {/* /.content-header */}
-        {/* Main content */}
-        <div className="content">
-            <div className="container-fluid">
-            <div className="row">
-                <div className="col-12">
-                {/* START CONTENT */}
-                <div className="form-inline">
-                    <p className="ml-auto flex-column text-right">
-                    <input type="text" className="form-control mr-1" placeholder="Document Number" onChange={handleInputChange} value={documentNum} />
-                    <button type="button" className="btn btn-primary mr-1" onClick={handleSearch} >Search</button>
-                    <button type="button" className="btn btn-default">Get more</button>
-                    </p>
-                </div>
+  return (
+    <div className="content-wrapper-x">
+      {/* Content Header (Page header) */}
+      <ContentHeader title={reviewType + " Adjustments"} />
+      {/* /.content-header */}
+      {/* Main content */}
+      <div className="content">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              {/* START CONTENT */}
+              <div className="form-inline">
+                <p className="ml-auto flex-column text-right">
+                  <input type="text" className="form-control mr-1" placeholder="Document Number" onChange={handleInputChange} value={documentNum} />
+                  <button type="button" className="btn btn-primary mr-1" onClick={handleSearch} >Search</button>
+                  <button type="button" className="btn btn-default">Get more</button>
+                </p>
+              </div>
 
-                <Tabs activeKey={activeTab} onSelect={(k) => setActiveTab(k)}>
-                    {/* <Tab eventKey="P36" title="P36">
-                        <div className="card">
-                            <div className="card-body">
-                                <div className="tab-content" id="custom-tabs-one-tabContent">
-                                    <div className="tab-pane fade show active" id="custom-tabs-one-adjustplus" role="tabpanel" aria-labelledby="custom-tabs-one-adjustplus-tab">
-                                        <p className="mb-4">Select adjustments you wish to Accept action o or Reject:</p>
-                                        
-                                        
-                                        <div className="row">
-                                            <div className="col-sm-4 col-lg-3">
-                                            <label>Document Number</label>
-                                            <div className="table-responsive" style={{height: 400, border: '1px solid #dee2e6'}}>
-                                                <table className="table table-as-list text-nowrap table-hover">
-                                                <tbody>
-                                                    {filterDocumentsByType('P36').map((doc, index) => (
-                                                        <tr key={index} onClick={ () => {handleSelectDocument(doc)} } className={ selectedDocument?.documentNum === doc.documentNum ? 'selected': '' } >
-                                                            <td>{doc.documentNum}</td>
-                                                        </tr>
-                                                    ))}
-                                                </tbody>
-                                                </table>
-                                            </div>
-                                            </div>
-                                            <div className="col-sm-8 col-lg-9">
-                                            <div className="row">
-                                                <div className="col-sm-6">
-                                                <div className="form-group">
-                                                    <label>Document Number</label>
-                                                    <input type="text" className="form-control" readOnly value={selectedDocument?.documentNum ?? ''} />
-                                                </div>
-                                                <div className="form-group">
-                                                    <label>Adjustment Location Code</label>
-                                                    <input type="text" className="form-control" readOnly value={selectedDocument?.homeLocationCode ?? ''} />
-                                                </div>
-                                                </div>
-                                                <div className="col-sm-6">
-                                                <div className="form-group">
-                                                    <label>Created By</label>
-                                                    <input type="text" className="form-control" readOnly value={selectedDocument?.createdByName ?? ''} />
-                                                </div>
-                                                <div className="form-group">
-                                                    <label>Created On</label>
-                                                    <input type="text" className="form-control" readOnly value={selectedDocument?.createdDtm ?? ''} />
-                                                </div>
-                                                </div>
-                                            </div>
-                                            <div className="table-responsive" style={{height: 300}}>
-                                                <table className="table table-head-fixed text-nowrap table-bordered table-hover">
-                                                <thead>
-                                                    <tr>
-                                                    <th>Account Number</th>
-                                                    <th>Service Number</th>
-                                                    <th>Adjustment Type</th>
-                                                    <th>Amount</th>
-                                                    <th>VAT</th>
-                                                    <th>Total</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    {adjustmentRequests.map((adj, index) => (
-                                                        <tr key={index}>
-                                                            <td>{adj.accountNum}</td>
-                                                            <td>{adj.serviceNum}</td>
-                                                            <td>{adj.adjustmentTypeName}</td>
-                                                            <td align='center'>{adj.disputeMny.toFixed(2)}</td>
-                                                            <td align='center'>{(adj.disputeMny * (  CPS_MAP_HASH[adj.cpsId]/100)).toFixed(2)}</td>
-                                                            <td align='center'>{(adj.disputeMny * (1+CPS_MAP_HASH[adj.cpsId]/100)).toFixed(2)}</td>
-                                                        </tr>
-                                                    ))}
-                                                </tbody>
-                                                </table>
-                                            </div>
-                                            </div>
-                                        </div>
-                                                                    
-                                        <div className="row">
-                                            <div className="col-sm-6">
-                                            <div className="form-group">
-                                                <label>Note</label>
-                                                <textarea className="form-control" rows={5} readOnly defaultValue={""} />
-                                            </div>
-                                            </div>
-                                            <div className="col-sm-6">
-                                            <div className="form-group">
-                                                <label>My Note</label>
-                                                <textarea className="form-control" rows={5} defaultValue={""} />
-                                            </div>
-                                            </div>
-                                        </div>
-                                        <div className="row">
-                                            <div className="col-12">
-                                            <div className="form-inline mt-4">
-                                                <p className="ml-auto mr-auto flex-column">
-                                                <button type="button" className="btn btn-primary mr-1" onClick={() => { handleUpdateDocumentStatus(selectedDocument, `${reviewType}-Accept`) }} >Accept Selected</button>
-                                                <button type="button" className="btn btn-default" onClick={() => { handleUpdateDocumentStatus(selectedDocument, `${reviewType}-Reject`) }} >Reject Selected</button>
-                                                </p>
-                                            </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </Tab> */}
+              <Tabs activeKey={activeTab} onSelect={handleTabSelect}>
+                <Tab eventKey="P35" title="P35">
+                  <ReviewDocType documentTypeDesc='P35' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
+                </Tab>
+                <Tab eventKey="P36" title="P36">
+                  <ReviewDocType documentTypeDesc='P36' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
+                </Tab>
+                <Tab eventKey="Adjust-" title="Adjust-">
+                  <ReviewDocType documentTypeDesc='Adjust-' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
+                </Tab>
+                <Tab eventKey="Adjust+" title="Adjust+">
+                  <ReviewDocType documentTypeDesc='Adjust+' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
+                </Tab>
+              </Tabs>
 
-                    <Tab eventKey="P35" title="P35">
-                        <ReviewDocType documentTypeDesc='P35' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
-                    </Tab>
-                    <Tab eventKey="P36" title="P36">
-                        <ReviewDocType documentTypeDesc='P36' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
-                    </Tab>
-                    <Tab eventKey="Adjust-" title="Adjust-">
-                        <ReviewDocType documentTypeDesc='Adjust-' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
-                    </Tab>
-                    <Tab eventKey="Adjust+" title="Adjust+">
-                        <ReviewDocType documentTypeDesc='Adjust+' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
-                    </Tab>
-                </Tabs>
-
-                {/* END CONTENT */}
-                </div>
+              {/* END CONTENT */}
             </div>
-            {/* /.row */}
-            </div>
-            {/* /.container-fluid */}
+          </div>
+          {/* /.row */}
         </div>
-        {/* /.content */}
-        </div>
-    );
-
+        {/* /.container-fluid */}
+      </div>
+      {/* /.content */}
+    </div>
+  );
 };
 
 export default Review;
--- src/pages/SearchAdj.js
+++ src/pages/SearchAdj.js
@@ -10,22 +10,13 @@ const SearchAdj = () => {
     const [selectedDocument, setSelectedDocument] = useState(null);
     const [adjustmentRequests, setAdjustmentRequests] = useState([]);
 
-    // const [documentStatus, setDocumentStatus] = useState(DOCUMENT_STATUS_LIST);
-    // const [selectedDocumentStatus, setSelectedDocumentStatus] = useState(null);
-
     const [searchBy, setSearchBy] = useState(SEARCH_BY_LIST);
     const [selectedSearchBy, setSelectedSearchBy] = useState(null);
-    
-    // const handleStatusChange = (e) => {
-    //   const selectedOption = documentStatus.find(status => status.value === e.target.value);
-    //   setSelectedDocumentStatus(selectedOption);
-    // };
 
     const handleSearchByChange = (e) => {
       const selectedOption = searchBy.find(search => search.value === e.target.value);
       setSelectedSearchBy(selectedOption);
     }
-
 
     const handleInputChange = (e) => {
       setDocumentNum(e.target.value);
@@ -35,7 +26,6 @@ const SearchAdj = () => {
       try {        
           const response = await api.get(`/api/Document/GetAllDocuments`, {
               params: {
-                  //documentStatus: selectedDocumentStatus?.value,
                   documentNum: documentNum,
                   searchBy: selectedSearchBy?.value
               }
@@ -47,9 +37,7 @@ const SearchAdj = () => {
     };
 
     const handleSearch = async () => {
-
         getAllDocuments();
-
         /* Clear selected document and adjustment requests */
         setSelectedDocument(null);
         setAdjustmentRequests([]);
@@ -87,21 +75,12 @@ const SearchAdj = () => {
                       <div className="col-sm-4">
                         <div className="form-group d-flex align-items-center">
                           <label style={{width: 160}}>Search for:</label>
-
-                          {/* <select className="form-control" onChange={(e) => handleStatusChange(e)} value={selectedDocumentStatus?.value}>
-                              <option value="">All</option>
-                              {documentStatus.map((status, index) => (
-                                <option key={index} value={status.value}>{status.name}</option>
-                              ))}
-                          </select> */}
-
                           <select className="form-control" onChange={(e) => handleSearchByChange(e)} value={selectedSearchBy?.value}>
                               <option value="">All</option>
                               {searchBy.map((search, index) => (
                                 <option key={index} value={search.value}>{search.name}</option>
                               ))}
                           </select>
-
                         </div>
                       </div>
                       <div className="col-sm-3">
@@ -132,7 +111,6 @@ const SearchAdj = () => {
                                 <th>Created by</th>
                                 <th>Created date</th>
                                 <th>Location</th>
-                                {/* <th>Status</th> */}
                               </tr>
                             </thead>
                             <tbody>
@@ -142,9 +120,8 @@ const SearchAdj = () => {
                                   <td>{document.documentTypeDesc}</td>
                                   <td>{document.totalMny}</td>
                                   <td>{document.createdByName}</td>
-                                  <td>{new Date(document.createdDtm).toLocaleString()}</td>
+                                  <td>{new Date(document.createdDtm).toLocaleString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' })}</td>
                                   <td>{document.homeLocationCode}</td>
-                                  {/* <td>{document.documentStatus}</td> */}
                                 </tr>
                               ))}
                             </tbody>
@@ -159,7 +136,7 @@ const SearchAdj = () => {
                           <input type="text" className="form-control" readOnly value={selectedDocument?.reviewedByName || ''} />
                         </div>
                         <div className="form-group">
-                          <input type="text" className="form-control" readOnly value={selectedDocument?.reviewedDtm ? new Date(selectedDocument.reviewedDtm).toLocaleString() : ''} />
+                          <input type="text" className="form-control" readOnly value={selectedDocument?.reviewedDtm ? new Date(selectedDocument.reviewedDtm).toLocaleString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' }) : ''} />
                         </div>
                       </div>
                       <div className="col-sm-6 col-md-4 col-lg-2">
@@ -168,7 +145,7 @@ const SearchAdj = () => {
                           <input type="text" className="form-control" readOnly value={selectedDocument?.approvedByName || ''} />
                         </div>
                         <div className="form-group">
-                          <input type="text" className="form-control" readOnly value={selectedDocument?.approvedDtm ? new Date(selectedDocument.approvedDtm).toLocaleString() : ''} />
+                          <input type="text" className="form-control" readOnly value={selectedDocument?.approvedDtm ? new Date(selectedDocument.approvedDtm).toLocaleString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' }) : ''} />
                         </div>
                       </div>
                       <div className="col-sm-6 col-md-4 col-lg-2">
@@ -177,7 +154,7 @@ const SearchAdj = () => {
                           <input type="text" className="form-control" readOnly value={selectedDocument?.financeReviewedByName || ''} />
                         </div>
                         <div className="form-group">
-                          <input type="text" className="form-control" readOnly value={selectedDocument?.financeReviewedDtm ? new Date(selectedDocument.financeReviewedDtm).toLocaleString() : ''} />
+                          <input type="text" className="form-control" readOnly value={selectedDocument?.financeReviewedDtm ? new Date(selectedDocument.financeReviewedDtm).toLocaleString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' }) : ''} />
                         </div>
                       </div>
                       <div className="col-sm-6 col-md-4 col-lg-2">
