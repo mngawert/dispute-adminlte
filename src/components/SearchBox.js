58a697626edd31680e8171d9b58abf681074b026
Montri <montrin@gmail.com>
2025-02-22 21:00:13 +0700

    Adjust search box position.

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -21,17 +21,6 @@ const SearchBox = ({ searchBy, selectedSearchBy, handleSearchByChange, documentN
                             <input type="text" className="form-control" onChange={handleInputChange} value={documentNum} disabled={!selectedSearchBy?.value} />
                         </div>
                     </div>
-                    <div className="col-sm-3">
-                        <div className="form-group d-flex align-items-center">
-                            <label style={{ width: 160 }}>Filter by:</label>
-                            <select className="form-control" onChange={handleFilterByChange} value={filterBy}>
-                                <option value="createdBy">Created By</option>
-                                <option value="reviewedBy">Reviewed By</option>
-                                <option value="approvedBy">Approved By</option>
-                                <option value="financeReviewedBy">Finance Reviewed By</option>
-                            </select>
-                        </div>
-                    </div>
                     <div className="col-sm-2">
                         <div className="form-group">
                             <button type="button" className="btn btn-primary" onClick={handleSearch}>Search</button>
@@ -39,6 +28,17 @@ const SearchBox = ({ searchBy, selectedSearchBy, handleSearchByChange, documentN
                     </div>
                 </div>
                 <div className="row">
+                    <div className="col-sm-3">
+                        <div className="form-group">
+                            <label>View Adjustmens that you :</label>
+                            <select className="form-control" onChange={handleFilterByChange} value={filterBy}>
+                                <option value="createdBy">Created</option>
+                                <option value="reviewedBy">Reviewed</option>
+                                <option value="approvedBy">Approved</option>
+                                <option value="financeReviewedBy">Finance Reviewed</option>
+                            </select>
+                        </div>
+                    </div>
                     <div className="col-sm-3">
                         <div className="form-group">
                             <label>From Date:</label>
