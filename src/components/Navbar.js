5ec0bef9f9a98cb56ffc8c509c55de3578563244
Montri <montrin@gmail.com>
2025-03-05 23:42:28 +0700

    Fix hamburger menu.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import $ from "jquery"; // Import jQuery
 
 export default function Navbar() {
 
@@ -7,42 +8,34 @@ export default function Navbar() {
     window.location.href = "/login"; // Redirect to login page
   };
 
+  const handleToggleSidebar = () => {
+    $("body").toggleClass("sidebar-collapse");
+  };
+
   return (
-    // <nav className="main-header navbar navbar-expand navbar-white navbar-light">
-    //   <ul className="navbar-nav">
-    //     <li className="nav-item">
-    //       <a className="nav-link" data-widget="pushmenu" href="#" role="button">
-    //         <i className="fas fa-bars"></i>
-    //       </a>
-    //       <button className="btn-xs info" onClick={() => handleLogout()}>logout</button>
-    //       </li>
-    //   </ul>
-    // </nav>
+    <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+      {/* Left navbar links */}
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" href="#" role="button" onClick={handleToggleSidebar}>
+            <i className="fas fa-bars"></i>
+          </a>
+        </li>
+        <li className="nav-item d-none d-sm-inline-block">
+          {/* <a href="../../index3.html" className="nav-link">Home</a> */}
+        </li>
+        <li className="nav-item d-none d-sm-inline-block">
+          {/* <a href="#" className="nav-link">Contact</a> */}
+        </li>
+      </ul>
+      {/* Right navbar links */}
+      <ul className="navbar-nav ml-auto">
+        {/* Navbar Search */}
+        <li className="nav-item">
+          {/* <a href="#" className="nav-link">User</a> */}
+        </li>
 
-
-<nav className="main-header navbar navbar-expand navbar-white navbar-light">
-  {/* Left navbar links */}
-  <ul className="navbar-nav">
-    <li className="nav-item">
-      <a className="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars" /></a>
-    </li>
-    <li className="nav-item d-none d-sm-inline-block">
-      {/* <a href="../../index3.html" className="nav-link">Home</a> */}
-    </li>
-    <li className="nav-item d-none d-sm-inline-block">
-      {/* <a href="#" className="nav-link">Contact</a> */}
-    </li>
-  </ul>
-  {/* Right navbar links */}
-  <ul className="navbar-nav ml-auto">
-    {/* Navbar Search */}
-    <li className="nav-item">
-    {/* <a href="#" className="nav-link">User</a> */}
-    </li>
-    {/* Messages Dropdown Menu */}
-  </ul>
-</nav>
-
-
+      </ul>
+    </nav>
   );
 }
