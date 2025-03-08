47122b13b58edb2f47bfe29c6dd5f28a05572a6e
Montri <montrin@gmail.com>
2025-03-08 23:15:15 +0700

    Add filter by service number.

--- src/components/InvoiceDataServices.js
+++ src/components/InvoiceDataServices.js
@@ -1,22 +1,43 @@
+import React, { useState } from 'react';
 
-const InvoiceDataServices = ({invoiceDataServices, selectedInvoiceDataService, handleSelectInvoiceServices}) => {
+const InvoiceDataServices = ({ invoiceDataServices, selectedInvoiceDataService, handleSelectInvoiceServices }) => {
+    const [filterText, setFilterText] = useState('');
 
-  return (
-    <>
-        <label>Service Number</label>
-        <div className="table-responsive" style={{height: 500, border: '1px solid #dee2e6'}}>
-            <table className="table table-as-list text-nowrap table-hover">
-            <tbody>
-                { invoiceDataServices.map((data, idx) => (
-                <tr key={idx} onClick={() => { handleSelectInvoiceServices(data); }} className={selectedInvoiceDataService?.serviceNumber === data.serviceNumber ? 'selected' : ''} >
-                    <td>{data.serviceNumber}</td>
-                </tr>
-                ))}
-            </tbody>
-            </table>
-        </div>    
-    </>
-  )
-}
+    const handleFilterChange = (e) => {
+        setFilterText(e.target.value);
+    };
+
+    const filteredInvoiceDataServices = invoiceDataServices.filter(data =>
+        data.serviceNumber.toLowerCase().includes(filterText.toLowerCase())
+    );
+
+    return (
+        <>
+            <label>Service Number</label>
+            <input
+                type="text"
+                className="form-control mb-3"
+                placeholder="Filter by Service Number"
+                value={filterText}
+                onChange={handleFilterChange}
+            />
+            <div className="table-responsive" style={{ height: 500, border: '1px solid #dee2e6' }}>
+                <table className="table table-as-list text-nowrap table-hover">
+                    <tbody>
+                        {filteredInvoiceDataServices.map((data, idx) => (
+                            <tr
+                                key={idx}
+                                onClick={() => { handleSelectInvoiceServices(data); }}
+                                className={selectedInvoiceDataService?.serviceNumber === data.serviceNumber ? 'selected' : ''}
+                            >
+                                <td>{data.serviceNumber}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
+};
 
 export default InvoiceDataServices;
