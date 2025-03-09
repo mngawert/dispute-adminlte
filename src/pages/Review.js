a5876ab574ff7daf48267499d9b9eb1d2072c05c
Montri <montrin@gmail.com>
2025-03-09 16:16:32 +0700

    Handle one record return in review.

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,29 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import api from '../api';
-import { CPS_MAP_HASH } from '../contexts/Constants';
 import { Tab, Tabs } from 'react-bootstrap';
 import ReviewDocType from './ReviewDocType';
 import ContentHeader from '../components/ContentHeader';
 
 const Review = ({ reviewType, prevDocumentStatus }) => {
 
-  console.log(reviewType);
-  console.log(prevDocumentStatus);
-
   const [documentNum, setDocumentNum] = useState('');
   const [documents, setDocuments] = useState([]);
   const [selectedDocument, setSelectedDocument] = useState({});
   const [adjustmentRequests, setAdjustmentRequests] = useState([]);
-
   const [activeTab, setActiveTab] = useState('P35');
 
   useEffect(() => {
     getAllDocuments();
   }, []);
-
-  const filterDocumentsByType = (type) => {
-    return documents.filter(doc => doc.documentTypeDesc === type);
-  };
 
   const handleInputChange = (e) => {
     setDocumentNum(e.target.value);
@@ -37,7 +28,18 @@ const Review = ({ reviewType, prevDocumentStatus }) => {
           documentNum: documentNum
         }
       });
-      setDocuments(response.data);
+      const fetchedDocuments = response.data;
+      setDocuments(fetchedDocuments);
+
+      if (fetchedDocuments.length === 1) {
+        const doc = fetchedDocuments[0];
+        setSelectedDocument(doc);
+        setActiveTab(doc.documentTypeDesc);
+        fetchAdjustmentRequests(doc.documentNum);
+      } else {
+        setSelectedDocument({});
+        setAdjustmentRequests([]);
+      }
     } catch (error) {
       console.error('Error fetching data', error);
     }
@@ -49,19 +51,21 @@ const Review = ({ reviewType, prevDocumentStatus }) => {
 
   const handleSelectDocument = async (doc) => {
     setSelectedDocument(doc);
+    fetchAdjustmentRequests(doc.documentNum);
+  };
 
+  const fetchAdjustmentRequests = async (documentNum) => {
     try {
       const response = await api.get(`/api/Adjustment/GetAdjustmentRequests`, {
         params: {
-          documentNum: doc.documentNum
+          documentNum: documentNum
         }
       });
-      console.log(response.data);
       setAdjustmentRequests(response.data);
     } catch (error) {
       console.error('Error fetching adjustment requests', error);
     }
-  }
+  };
 
   const handleUpdateDocumentStatus = async (doc, _documentStatus, note, resetMyNote) => {
     try {
@@ -71,14 +75,13 @@ const Review = ({ reviewType, prevDocumentStatus }) => {
         note: note,
         updatedBy: JSON.parse(localStorage.getItem('userLogin'))?.userId
       });
-      console.log(response.data);
       resetPage();
       getAllDocuments();
       resetMyNote(); // Reset myNote after updating document status
     } catch (error) {
       console.error('Error updating document status', error);
     }
-  }
+  };
 
   const resetPage = () => {
     setDocumentNum('');
@@ -108,7 +111,6 @@ const Review = ({ reviewType, prevDocumentStatus }) => {
                 <p className="ml-auto flex-column text-right">
                   <input type="text" className="form-control mr-1" placeholder="Document Sequence" onChange={handleInputChange} value={documentNum} />
                   <button type="button" className="btn btn-primary mr-1" onClick={handleSearch} >Search</button>
-                  {/* <button type="button" className="btn btn-default">Get more</button> */}
                 </p>
               </div>
 
@@ -119,12 +121,6 @@ const Review = ({ reviewType, prevDocumentStatus }) => {
                 <Tab eventKey="P36" title="P36">
                   <ReviewDocType documentTypeDesc='P36' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
                 </Tab>
-                {/* <Tab eventKey="Adjust-" title="Adjust-">
-                  <ReviewDocType documentTypeDesc='Adjust-' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
-                </Tab>
-                <Tab eventKey="Adjust+" title="Adjust+">
-                  <ReviewDocType documentTypeDesc='Adjust+' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
-                </Tab> */}
               </Tabs>
 
               {/* END CONTENT */}
