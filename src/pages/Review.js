877eb492b06a619e4789e5b98cbf9711ae2df37f
Montri <montrin@gmail.com>
2025-05-25 22:47:18 +0700

    Add B+- in review.

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -143,6 +143,9 @@ const Review = ({ reviewType, prevDocumentStatus }) => {
                 <Tab eventKey="P3+" title="P3+">
                   <ReviewDocType documentTypeDesc='P3+' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
                 </Tab>
+                <Tab eventKey="B+-" title="B+/-">
+                  <ReviewDocType documentTypeDesc='B+-' documents={documents} adjustmentRequests={adjustmentRequests} selectedDocument={selectedDocument} reviewType={reviewType} handleSelectDocument={handleSelectDocument} handleUpdateDocumentStatus={handleUpdateDocumentStatus} />
+                </Tab>
               </Tabs>
 
               {/* END CONTENT */}
