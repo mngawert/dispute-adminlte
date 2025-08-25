0dc5dab9ec7874bd30fb67a65fc47eff91b00fb0
Montri <montrin@gmail.com>
2025-08-25 20:44:21 +0700

    Add profile icon

--- src/components/ContentHeader.js
+++ src/components/ContentHeader.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import './ContentHeader.css'; // We'll create this file for custom styling
 
 const ContentHeader = ({ title }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  
   const userLogin = JSON.parse(localStorage.getItem('userLogin'));
   const username = userLogin?.username;
   const homeLocationCode = userLogin?.homeLocationCode;
@@ -10,7 +14,25 @@ const ContentHeader = ({ title }) => {
   
   // Combine Thai name parts, only if at least one of them exists
   const thaiNameDisplay = (titleTh || firstNameTh || lastNameTh) ? 
-    `[ ${titleTh} ${firstNameTh} ${lastNameTh} ]` : '';
+    `${titleTh} ${firstNameTh} ${lastNameTh}` : '';
+    
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    };
+    
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
   return (
     <div className="content-header">
@@ -20,11 +42,39 @@ const ContentHeader = ({ title }) => {
             <h1 className="m-0">{title}</h1>
           </div>{/* /.col */}
           <div className="col-sm-6">
-            <ol className="breadcrumb float-sm-right">
-              <li className="breadcrumb-item active">
-                [ {username} ] [ {homeLocationCode} ] {thaiNameDisplay}
-              </li>
-            </ol>
+            <div className="float-sm-right user-profile-container" ref={dropdownRef}>
+              <div className="user-profile-icon" onClick={toggleDropdown}>
+                <div className="user-avatar">
+                  <i className="fas fa-user"></i>
+                </div>
+              </div>
+              {dropdownOpen && (
+                <div className="user-profile-dropdown">
+                  <div className="user-profile-header">
+                    <div className="user-avatar-large">
+                      <i className="fas fa-user"></i>
+                    </div>
+                    <h5>{username}</h5>
+                  </div>
+                  <div className="user-profile-details">
+                    <div className="user-detail-item">
+                      <i className="fas fa-user mr-2"></i>
+                      <span>Username: {username}</span>
+                    </div>
+                    <div className="user-detail-item">
+                      <i className="fas fa-map-marker-alt mr-2"></i>
+                      <span>Location: {homeLocationCode}</span>
+                    </div>
+                    {thaiNameDisplay && (
+                      <div className="user-detail-item">
+                        <i className="fas fa-id-card mr-2"></i>
+                        <span>Name: {thaiNameDisplay}</span>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              )}
+            </div>
           </div>{/* /.col */}
         </div>{/* /.row */}
       </div>{/* /.container-fluid */}
