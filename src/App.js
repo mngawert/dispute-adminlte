b18a6adbc1a5e99b50432d070b1aaa3219db3ec6
Montri <montrin@gmail.com>
2025-09-23 07:49:00 +0700

    Enable Approver to access Cancel link.

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
             <Route path="/Review" element={ <PrivateRoute element={<Review reviewType="Review" prevDocumentStatus='Create-Accept' />} allowedRoles={['Admin', 'Reviewer']} /> } />
             <Route path="/Approve" element={ <PrivateRoute element={<Review reviewType="Approve" prevDocumentStatus='Review-Accept' />} allowedRoles={['Admin', 'Approver']} /> } />
             <Route path="/Finance" element={ <PrivateRoute element={<Review reviewType="Finance" prevDocumentStatus='Approve-Accept' />} allowedRoles={['Admin', 'Finance']} /> } />
-            <Route path="/Cancel" element={ <PrivateRoute element={<Review reviewType="Cancel" prevDocumentStatus='CREATED_IN_RBM' />} allowedRoles={['Admin', 'Finance']} /> } />
+            <Route path="/Cancel" element={ <PrivateRoute element={<Review reviewType="Cancel" prevDocumentStatus='CREATED_IN_RBM' />} allowedRoles={['Admin', 'Finance', 'Approver']} /> } />
             <Route path="/Retry" element={ <PrivateRoute element={<Review reviewType="Retry" prevDocumentStatus='ERROR_IN_RBM,PARTIAL_ERROR' />} allowedRoles={['Admin', 'Approver', 'Finance']} /> } />
             {/* <Route path="/reviewBak" element={<ReviewBak />} /> */}
             <Route path="/AdjustPlus" element={ <PrivateRoute element={<AdjustPlus />} allowedRoles={['Admin', 'Creator', 'Creator_Adjust+']} /> } />
